09fd2bf0fc976c9317e6748c193c664d42570d63
hinsongarion-ui <hinsongarion@gmail.com>
2025-10-30 13:09:27 -0500

    name age hobby

---
+++ # Ask the user for their information.py
@@ -0,0 +1,10 @@
+# Ask the user for their information
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+hobby = input("Enter your favorite hobby: ")
+
+# Print the information
+print("\nHere is the information you entered:")
+print("Name:", name)
+print("Age:", age)
+print("Favorite Hobby:", hobby)
